fb9f0876be1fc4554e4673a30888658b21d7215a
robertalanis <rxalanis@gmail.com>
2020-04-12 20:13:48 -0500

    tested all of Letter.js functions

--- Letter.js
+++ Letter.js
@@ -6,19 +6,27 @@ var Letter = function(character){
     this.guessed = false;
     this.toString = function () {
         //Returns the underlying character if the letter has been guessed
-        if (this.guessed = true) {return this.letter;}
-        //Returnsor a placeholder (underscore) if the letter has not been guessed
+        if (this.guessed) {return this.letter;}
+        //Returns a placeholder (underscore) if the letter has not been guessed
         else {return "_";}
     }
-    this.checkGuess = function (guess){
+    this.checkGuess = function (userGuess){
         //Takes a character as an argument and checks it against the underlying character
-        if (guess === this.letter) 
+        if (userGuess === this.letter) 
         //Updates the stored boolean value to true if it was guessed correctly
         {this.guessed = true;}
     }
 }
 
-console.log("Test: Letter.js")
+//var p = new Letter("p")
+//console.log(p.letter);
+//console.log(p.guessed);
+//console.log("X" +p.toString()+ "X")
+//p.checkGuess("p")
+//console.log(p.guessed);
+//console.log("X" +p.toString()+ "X")
 
 // exporting our Letter constructor
 module.exports = Letter;
+
+
