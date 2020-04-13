a4555c3a16fd551f155b64277c438f6c72f70fcc
robertalanis <rxalanis@gmail.com>
2020-04-13 16:05:58 -0500

    alert user if they won or lost and asks to start a new game

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var Word = require("./Word");
 
 var inquirer = require("inquirer");
 var Table = require("cli-table3");
+const chalk = require("chalk");
 var clear = require("clear");
 
 let wordBank = [
   "argentina",
-  "belize",
   "bolivia",
   "brazil",
   "chile",
@@ -17,80 +17,139 @@ let wordBank = [
   "ecuador",
   //'el salvador',
   "guatemala",
-  "guyana",
-  "haiti",
   "honduras",
   "mexico",
   "nicaragua",
   "panama",
   "paraguay",
   "peru",
-  "suriname",
   "uruguay",
   "venezuela",
 ];
 
+//Starts game
 function newGame() {
+  //clear the terminal
   clear();
-  //Pick a random word from the word bank.
-  var word = new Word(wordBank[Math.floor(Math.random() * wordBank.length)]);
+  //Pick a random word from the word bank and store it.
+  var answer = wordBank[Math.floor(Math.random() * wordBank.length)];
+  //Pass our answer through our Word constructor
+  var word = new Word(answer);
   //Create an array of new Letter objects representing the letters of the underlying word
   word.wordToArray();
+
+  let inputArray = [];
+
+  //Set incorrect guess limit
+  let incorrectAttempts = 5;
+
+  //Call our display function
+  display();
+  //Call our status function
+  checkStatus();
 
   //Function to display our word on a table
   function display() {
     var displayTable = new Table({ wordWrap: true });
     displayTable.push(
-      [{ hAlign: "center", content: "Guess the Latin American country below:" }],
+      [
+        {
+          hAlign: "center",
+          content: "Guess the Latin American country below:",
+        },
+      ],
       [{ hAlign: "center", content: word.displayString() }]
     );
     console.log(displayTable.toString());
   }
-  //Call our display function
-  display();
 
   //Function to check gameplay status
   function checkStatus() {
     // if there are no underscores the user has won
     if (word.displayString().split("_").length - 1 === 0) {
       isWinner();
+    } else if (incorrectAttempts === 0) {
+      isLoser();
     } else {
       makeGuess();
     }
   }
-  //Call our status function
-  checkStatus();
+
   //Function to prompt our user to make a guess
   function makeGuess() {
     inquirer
       .prompt([
         {
-          /* Pass your questions in here */
           type: "input",
           name: "guess",
           message: "Guess a letter: ",
         },
       ])
       .then((answers) => {
-        // Use user feedback for... whatever!!
         var before = word.displayString().split("_").length - 1;
         word.checkGuess(answers.guess);
         var after = word.displayString().split("_").length - 1;
         clear();
         display();
         if (before === after) {
-          console.log("incorrect");
+          incorrectAttempts--;
+          console.log(chalk.red("incorrect"));
+          console.log(
+            chalk.red(
+              "you have " + incorrectAttempts + " incorrect attempts left!"
+            )
+          );
         } else {
-          console.log("correct");
+          console.log(chalk.green("correct"));
         }
-
         checkStatus();
       });
   }
+  //If user wins
+  function isWinner() {
+    //Congratulate user
+    console.log("Good Job! You win!");
+    //Ask to play again
+    playAgain();
+  }
+  //If user loses
+  function isLoser() {
+    //Give correct answer
+    console.log("Sorry! You lost! The correct answer was " + answer);
+    //Ask to play again
+    playAgain();
+  }
 }
-
-function isWinner() {
-  console.log("Good Job! You win!");
+//Gives user option to play another game
+function playAgain() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "response",
+        choices: ["yes", "no"],
+        message: "Would you like to play again?",
+      },
+    ])
+    .then((answers) => {
+      //Starts new game
+      if (answers.response === "yes") {
+        newGame();
+      //Ends game  
+      } else {
+        //Clear console
+        clear();
+        //Thank you message
+        var thankYouTable = new Table({ wordWrap: true });
+        thankYouTable.push([
+          {
+            hAlign: "center",
+            content: chalk.cyan("Thanks for playing!"),
+          },
+        ]);
+        console.log(thankYouTable.toString());
+      }
+    });
 }
-
+//Run game
 newGame();
